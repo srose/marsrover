ea50e53194ea46124b93e16d7676a4b56d600ba3
Sebastian Rose <sebastian.rose@gmail.com>
2023-03-20 14:49:47 +0100

    turn left, right done

--- src/main/java/domain/Heading.java
+++ src/main/java/domain/Heading.java
@@ -1,5 +1,31 @@
 package domain;
 
 public class Heading {
+    private CardinalDirection direction;
 
+    public Heading(CardinalDirection direction) {
+        this.direction = direction;
+    }
+
+    public void turnRight() {
+        this.direction = switch (this.direction) {
+            case NORTH -> CardinalDirection.EAST;
+            case SOUTH -> CardinalDirection.WEST;
+            case WEST -> CardinalDirection.NORTH;
+            case EAST -> CardinalDirection.SOUTH;
+        };
+    }
+
+    public CardinalDirection getDirection() {
+        return direction;
+    }
+
+    public void turnLeft() {
+        this.direction = switch (this.direction) {
+            case NORTH -> CardinalDirection.WEST;
+            case SOUTH -> CardinalDirection.EAST;
+            case WEST -> CardinalDirection.SOUTH;
+            case EAST -> CardinalDirection.NORTH;
+        };
+    }
 }
--- src/test/java/domain/HeadingTest.java
+++ src/test/java/domain/HeadingTest.java
@@ -1,20 +1,57 @@
 package domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class HeadingTest {
 
-    @Test
-    void starting_turning_right_is_correct() {
-        var heading = new Heading(CardinalDirection.NORTH);
+    @ParameterizedTest()
+    @MethodSource("cardinalDirectionCombinationsTurnRight")
+    void starting_turning_right_is_correct(CardinalDirection starting, CardinalDirection result) {
+        var heading = new Heading(starting);
 
         heading.turnRight();
 
         var direction = heading.getDirection();
-        assertThat(direction).isEqualTo(CardinalDirection.EAST);
 
+        assertThat(direction).isEqualTo(result);
     }
+
+    private static Stream<Arguments> cardinalDirectionCombinationsTurnRight() {
+        return Stream.of(
+                arguments(CardinalDirection.NORTH, CardinalDirection.EAST),
+                arguments(CardinalDirection.EAST, CardinalDirection.SOUTH),
+                arguments(CardinalDirection.SOUTH, CardinalDirection.WEST),
+                arguments(CardinalDirection.WEST, CardinalDirection.NORTH)
+        );
+    };
+
+    @ParameterizedTest()
+    @MethodSource("cardinalDirectionCombinationsTurnLeft")
+    void starting_turning_left_is_correct(CardinalDirection starting, CardinalDirection result) {
+        var heading = new Heading(starting);
+
+        heading.turnLeft();
+
+        var direction = heading.getDirection();
+
+        assertThat(direction).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> cardinalDirectionCombinationsTurnLeft() {
+        return Stream.of(
+                arguments(CardinalDirection.NORTH, CardinalDirection.WEST),
+                arguments(CardinalDirection.EAST, CardinalDirection.NORTH),
+                arguments(CardinalDirection.SOUTH, CardinalDirection.EAST),
+                arguments(CardinalDirection.WEST, CardinalDirection.SOUTH)
+        );
+    };
 
 }

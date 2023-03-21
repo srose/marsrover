b1c3659a8a72b5a728cf09a8d0b566e7c4ae2885
Sebastian Rose <sebastian.rose@gmail.com>
2023-03-21 13:38:18 +0100

    Refactored rover to have the dependency Heading injected

--- src/main/java/domain/Rover.java
+++ src/main/java/domain/Rover.java
@@ -4,11 +4,17 @@ public class Rover {
 
     private CardinalDirection direction;
 
-    private final Heading heading = new Heading();
+    private final Heading heading;
 
     public Rover(CardinalDirection startDirection) {
         direction = startDirection;
+        heading = new Heading();
     }
+    public Rover(CardinalDirection startDirection, Heading startHeading) {
+        direction = startDirection;
+        heading = startHeading;
+    }
+
 
     public CardinalDirection getDirection() {
         return this.direction;
